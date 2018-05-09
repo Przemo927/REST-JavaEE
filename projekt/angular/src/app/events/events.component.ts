91c669fa9f09898ba078eaf6af26efe86505625e
Przemek <przemo927@wp.pl>
2018-05-09 18:08:50 +0200

    Add various opacity style to buttons.

--- projekt/angular/src/app/events/events.component.ts
+++ projekt/angular/src/app/events/events.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { EventService } from '../event.service';
+import {Component, OnInit} from "@angular/core";
+import {EventService} from "../event.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-events',
@@ -8,14 +9,34 @@ import { EventService } from '../event.service';
 })
 export class EventsComponent implements OnInit {
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService,private router:Router) { }
 
   private events;
+  private cityStyle;
+  private positionStyle;
+  private allStyle;
 
 
 ngOnInit() {
-
+  this.positionStyle={'opacity':'0.5'};
+  this.cityStyle={'opacity':'0.5'};
+  this.allStyle={'opacity':'1'};
 }
+  searchByPosition():void {
+    this.positionStyle={'opacity':'1'};
+    this.cityStyle={'opacity':'0.5'};
+    this.allStyle={'opacity':'0.5'};
+  }
+  searchByCity():void {
+    this.positionStyle={'opacity':'0.5'};
+    this.cityStyle={'opacity':'1'};
+    this.allStyle={'opacity':'0.5'};
+  }
+  showAllEvents():void {
+    this.positionStyle={'opacity':'0.5'};
+    this.cityStyle={'opacity':'0.5'};
+    this.allStyle={'opacity':'1'};
+  }
 
 getEventsByNameOfCity(nameOfCity:string){
     return this.eventService.getEventsByNameOfCity(nameOfCity).subscribe(events=>this.events=events);
