ccf3626fb22f8c3ddca9335c4fbab602d23e2ca5
Przemek <przemo927@wp.pl>
2018-05-10 21:53:43 +0200

    Add Page Service.

---
+++ projekt/angular/src/app/page.service.ts
@@ -0,0 +1,108 @@
+import { Injectable } from '@angular/core';
+import {Page} from './page';
+import {Observable} from 'rxjs/Rx';
+import {Discovery} from './discovery';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable()
+export class PageService {
+  actualPage: Page= null;
+  preparedPages: Page[];
+  private url: string;
+  constructor(public http: HttpClient, url: string) {
+    this.url = url;
+  }
+
+  preparePages(quantityOnOnePage: number, quantityOfAllElements: number): Page[] {
+    let pages = 0;
+    if ((quantityOfAllElements % quantityOnOnePage) === 0) {
+      pages = quantityOfAllElements / quantityOnOnePage;
+    }else {
+      pages = Math.floor(quantityOfAllElements / quantityOnOnePage) + 1;
+    }
+    this.preparedPages = Array(pages).fill(0).map((x, i) => new Page(i + 1, i * quantityOnOnePage, quantityOnOnePage));
+
+    return this.preparedPages;
+  }
+  getElementsForActualPage<T>(numberOfActualPage: number): Observable<T[]> {
+    if (this.actualPage == null || (this.actualPage != null && this.actualPage.numberPage !== numberOfActualPage)) {
+      this.giveActualPage(numberOfActualPage);
+      return this.getElementsWithLimit<T>(this.actualPage.firstIndex, this.actualPage.quantity);
+    }else {
+      return Observable.empty<T[]>();
+
+    }
+  }
+
+  markFirstPage():any {
+    let currentPage= document.getElementById('1');
+    currentPage.style.backgroundColor = "#ddd";
+    return currentPage;
+  }
+  changeBackGroundColorOfPage(currentPage,event):any{
+    if (event.target.classList.contains('glyphicon-chevron-left')) {
+      if (currentPage.id > 1) {
+        currentPage.style.backgroundColor = 'white';
+        currentPage = document.getElementById((+currentPage.id - 1).toString());
+        currentPage.style.backgroundColor = '#ddd';
+      }
+    } else if (event.target.classList.contains('glyphicon-chevron-right')) {
+      if (currentPage.id < this.preparedPages.length) {
+        currentPage.style.backgroundColor = 'white';
+        currentPage = document.getElementById((+currentPage.id + 1).toString());
+        currentPage.style.backgroundColor = '#ddd';
+      }
+    } else if (currentPage != null && currentPage !== event.target) {
+      currentPage.style.backgroundColor = 'white';
+      currentPage = event.target;
+      currentPage.style.backgroundColor = '#ddd';
+    }else {
+      currentPage = event.target;
+      currentPage.style.backgroundColor = '#ddd';
+    }
+    return currentPage;
+  }
+  getElementsWithLimit<T>(firstIndex: number, quantity: number): Observable<T[]> {
+    return this.http.get<T[]>(this.url + '?beginWith=' + firstIndex + '&quantity=' + quantity);
+  }
+  chooseFirstPage<T>(): Observable<T[]> {
+    this.giveActualPage(1);
+    console.log(this.actualPage.firstIndex, this.actualPage.quantity);
+    return this.getElementsWithLimit<T>(this.actualPage.firstIndex, this.actualPage.quantity);
+  }
+
+  increaseLimit<T>(): Observable<T[]> {
+    if (this.actualPage != null && this.actualPage.numberPage < this.preparedPages.length) {
+      this.giveNextPage();
+      console.log(this.actualPage.firstIndex, this.actualPage.quantity);
+      return this.getElementsWithLimit<T>(this.actualPage.firstIndex, this.actualPage.quantity);
+    }else {
+      return Observable.empty<T[]>();
+    }
+  }
+
+  decreaseLimit<T>(): Observable<T[]> {
+    if (this.actualPage != null && this.actualPage.numberPage > 1) {
+      this.givePreviousPage();
+      console.log(this.actualPage.firstIndex, this.actualPage.quantity);
+      return this.getElementsWithLimit<T>(this.actualPage.firstIndex, this.actualPage.quantity);
+    }else {
+      return Observable.empty<T[]>();
+    }
+  }
+
+  getQuantityOfAllElements(): Observable<any> {
+    return this.http.get<any>(this.url + '/quantity');
+  }
+  private givePreviousPage(): void {
+    this.actualPage = this.preparedPages[this.actualPage.numberPage - 2];
+  }
+
+  private giveNextPage(): void {
+    this.actualPage = this.preparedPages[this.actualPage.numberPage];
+  }
+
+  private giveActualPage(numberOfActualPage: number): void {
+    this.actualPage = this.preparedPages[numberOfActualPage - 1];
+  }
+}
---
+++ projekt/angular/src/app/page.ts
@@ -0,0 +1,11 @@
+export class Page {
+    numberPage:number;
+    firstIndex:number;
+    quantity:number;
+    
+    constructor(numberPage: number, firstIndex: number, quantity:number){
+    this.numberPage=numberPage;
+    this.firstIndex=firstIndex;
+    this.quantity=quantity;
+   }
+}
