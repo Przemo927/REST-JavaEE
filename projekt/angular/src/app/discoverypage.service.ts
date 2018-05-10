bfe0f43a93092ebe8315698bbf35b589608f0d61
Przemek <przemo927@wp.pl>
2018-05-10 21:46:45 +0200

    Add Page Service.

---
+++ projekt/angular/src/app/discoverypage.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { DiscoveryService } from './discovery.service';
+import { Page } from './page';
+import { Discovery } from './discovery';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from 'rxjs/Rx';
+import { PageService } from './page.service';
+
+@Injectable()
+export class DiscoveryPageService extends PageService {
+
+    constructor(public http: HttpClient) {
+    super(http, 'http://localhost:8080/projekt/api/discovery');
+  }
+
+
+}
