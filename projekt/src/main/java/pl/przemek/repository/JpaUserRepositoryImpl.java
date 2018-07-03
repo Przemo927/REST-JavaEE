c9cd338346d64e1b8514802dfea4a30e8d8cd98a
Przemek <przemo927@wp.pl>
2018-07-03 21:31:03 +0200

    Save sql queries as final static variables.

--- projekt/src/main/java/pl/przemek/repository/JpaUserRepositoryImpl.java
+++ projekt/src/main/java/pl/przemek/repository/JpaUserRepositoryImpl.java
@@ -12,53 +12,50 @@ import pl.przemek.model.User;
 
 @Stateless
 public class JpaUserRepositoryImpl extends JpaRepository<User> implements JpaUserRepository  {
-	
-@PersistenceContext
-private EntityManager em;
 
 @Override
 @PermitAll
 public void add(User user) {
     em.persist(user);
 	}
-
-
-@RolesAllowed("admin")
-public Integer updateWithoutPassword(User user) {
-    Query queryUpdateUser = em.createNamedQuery("User.editUser");
-    queryUpdateUser.setParameter("username", user.getUsername());
-    queryUpdateUser.setParameter("email", user.getEmail());
-    queryUpdateUser.setParameter("active", user.isActive());
-    queryUpdateUser.setParameter("id", user.getId());
-    return queryUpdateUser.executeUpdate();
-}
-
-    @RolesAllowed({"admin","user"})
-public List<User> getUserByUsername(String username) {
-	TypedQuery<User> getAllQuery = em.createNamedQuery("User.findByUsername", User.class);
-	getAllQuery.setParameter("username",username);
-    List<User> users = getAllQuery.getResultList();
-    return users;
-	
-}
-@PermitAll
-public boolean checkPresenceOfUserByUsername(String username){
-    Query query=em.createNativeQuery("SELECT 1 FROM User WHERE username=:username");
-    query.setParameter("username",username);
-    List list=query.getResultList();
-    if(list.size()!=0){
-        return false;
+    private final static String PRESENCE_OF_USERNAME="SELECT 1 FROM User WHERE username=:username";
+    private final static String PRESENCE_OF_EMAIL="SELECT 1 FROM User WHERE email=:email";
+    @RolesAllowed("admin")
+    public Integer updateWithoutPassword(User user) {
+        Query queryUpdateUser = em.createNamedQuery("User.editUser");
+        queryUpdateUser.setParameter("username", user.getUsername());
+        queryUpdateUser.setParameter("email", user.getEmail());
+        queryUpdateUser.setParameter("active", user.isActive());
+        queryUpdateUser.setParameter("id", user.getId());
+        return queryUpdateUser.executeUpdate();
     }
-    return true;
-}
+
+        @RolesAllowed({"admin","user"})
+    public List<User> getUserByUsername(String username) {
+        TypedQuery<User> getAllQuery = em.createNamedQuery("User.findByUsername", User.class);
+        getAllQuery.setParameter("username",username);
+        List<User> users = getAllQuery.getResultList();
+        return users;
+
+    }
     @PermitAll
-public boolean checkPresenceOfEmail(String email){
-        Query query=em.createNativeQuery("SELECT 1 FROM User WHERE email=:email ");
-        query.setParameter("email",email);
+    public boolean checkPresenceOfUserByUsername(String username){
+        Query query=em.createNativeQuery(PRESENCE_OF_USERNAME);
+        query.setParameter("username",username);
         List list=query.getResultList();
         if(list.size()!=0){
             return false;
         }
         return true;
     }
+        @PermitAll
+    public boolean checkPresenceOfEmail(String email){
+            Query query=em.createNativeQuery(PRESENCE_OF_EMAIL);
+            query.setParameter("email",email);
+            List list=query.getResultList();
+            if(list.size()!=0){
+                return false;
+            }
+            return true;
+        }
 }
