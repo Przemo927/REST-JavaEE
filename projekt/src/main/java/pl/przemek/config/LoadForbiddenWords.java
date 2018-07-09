24b3d439eda4cb167eb0c7a89a17edd33b3502de
Przemek <przemo927@wp.pl>
2018-07-09 17:40:09 +0200

    Add table ForbiddenWords, txt file to fill this table. Add sql query to load data from this file.

---
+++ projekt/src/main/java/pl/przemek/config/LoadForbiddenWords.java
@@ -0,0 +1,31 @@
+package pl.przemek.config;
+
+
+import pl.przemek.Store.ProhibitedWordsStore;
+import pl.przemek.model.ForbiddenWords;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Singleton
+@Startup
+public class LoadForbiddenWords {
+
+    @PersistenceContext
+    private EntityManager em;
+    @Inject
+    private ProhibitedWordsStore store;
+
+    @PostConstruct
+    public void loadWords(){
+        TypedQuery<ForbiddenWords> query=em.createNamedQuery("ProhibitedWords.SelectAll",ForbiddenWords.class);
+        List<ForbiddenWords> listOfWords=query.getResultList();
+        store.setListOfForbiddenWords(listOfWords);
+    }
+}
