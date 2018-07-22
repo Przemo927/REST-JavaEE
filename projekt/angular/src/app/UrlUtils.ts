3f258889d0bbec9608a15ca78d74ffec6068c130
Przemek <przemo927@wp.pl>
2018-07-22 22:07:44 +0200

    Add utility methods for handling urls.

---
+++ projekt/angular/src/app/UrlUtils.ts
@@ -0,0 +1,25 @@
+export class UrlUtils {
+  public static addParameterToUrl(url:string,parameter:string,value:any):string{
+    if(url.includes("?")){
+      url+="&"+parameter+"="+value;
+    }else{
+      url+="?"+parameter+"="+value;
+    }
+    return url;
+  }
+  public static deleteParameterFromUrl(url:string,nameOfParameter:string):string{
+    let regExp=new RegExp('[?&]'+nameOfParameter+'=.[^&?]*');
+    if(url.match(regExp)!==null){
+      let splittedUrl=url.split(regExp);
+      if(!splittedUrl[0].includes('?')){
+        splittedUrl[1].replace('&','?');
+      }
+      return splittedUrl.join('');
+    }
+    return url;
+  }
+  public static checkUrlWithRegex(url:string, regexp:string):boolean{
+    let regExp:RegExp=new RegExp(regexp);
+    return regExp.test(url);
+  }
+}
