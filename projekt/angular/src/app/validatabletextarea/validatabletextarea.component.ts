a4fe1e7c0f4af7c4dd531db9802507c874ea7585
Przemek <przemo927@wp.pl>
2018-07-29 15:04:31 +0200

    improvement of worki validatabletextarea component

--- projekt/angular/src/app/validatabletextarea/validatabletextarea.component.ts
+++ projekt/angular/src/app/validatabletextarea/validatabletextarea.component.ts
@@ -1,13 +1,14 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
 
 @Component({
   selector: 'app-validatabletextarea',
   templateUrl: './validatabletextarea.component.html',
   styleUrls: ['./validatabletextarea.component.css']
 })
-export class ValidatabletextareaComponent implements OnInit {
+export class ValidatabletextareaComponent implements OnInit, AfterViewInit {
 
   @Input() placeholder:string;
+  @Input() idContainer:string;
   @Input() rows:number;
   @Input() positionMessage:string;
   private textArea;
@@ -16,16 +17,22 @@ export class ValidatabletextareaComponent implements OnInit {
   @Output() changeEvent: EventEmitter<string> = new EventEmitter<string>();
   constructor() {
   }
-
-  ngOnInit() {
-    this.textArea=document.getElementById("textArea");
+  ngAfterViewInit(){
+    let container=document.getElementById(this.idContainer);
+    this.textArea=container.getElementsByTagName("textArea")[0];
     if(this.positionMessage==="above"){
-      document.getElementById("container").style.flexDirection='column-reverse';
+      container.setAttribute("style","flex-direction: column-reverse");
+    }else{
+      container.setAttribute("style","flex-direction: column");
     }
+    container.style.display='flex';
+  }
+  ngOnInit() {
   }
 
  setWrong(){
     this.textArea.classList.remove("highlight");
+   //need to reload keyframes
     void this.textArea.offsetWidth;
     this.textArea.classList.add("highlight");
  }
