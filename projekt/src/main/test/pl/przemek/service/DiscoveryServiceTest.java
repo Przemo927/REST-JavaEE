002d2b31ae96a48fde55daea4ff8d92911922bb8
Przemek <przemo927@wp.pl>
2018-08-08 16:10:01 +0200

    Adapt DiscoveryServiceTest and use inMemore layer.

--- projekt/src/main/test/pl/przemek/service/DiscoveryServiceTest.java
+++ projekt/src/main/test/pl/przemek/service/DiscoveryServiceTest.java
@@ -8,12 +8,16 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import pl.przemek.model.Discovery;
+import pl.przemek.model.Vote;
 import pl.przemek.repository.JpaDiscoveryRepository;
 import pl.przemek.repository.JpaVoteRepository;
+import pl.przemek.repository.inMemoryRepository.JpaDiscoveryRepositoryInMemoryImpl;
+import pl.przemek.repository.inMemoryRepository.JpaVoteRepositoryInMemoryImpl;
 
 import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import static junitparams.JUnitParamsRunner.$;
@@ -46,6 +50,39 @@ public class DiscoveryServiceTest {
     @Rule
     public ExpectedException thrown=ExpectedException.none();
 
+    private void createDiscoveryServiceObjectWithInMemoryLayer(){
+        discRepo=new JpaDiscoveryRepositoryInMemoryImpl();
+        voteRepo=new JpaVoteRepositoryInMemoryImpl();
+        discoveryService=new DiscoveryService(discRepo,voteRepo);
+    }
+
+    @Test
+    public void shouldDoNotAddDiscoveryWhenIsNull(){
+        createDiscoveryServiceObjectWithInMemoryLayer();
+        int amountOfDiscoveriesBeforeAdd=discRepo.getAll("",Discovery.class).size();
+
+        discoveryService.addDiscovery(null);
+
+        assertTrue(discRepo.getAll("",Discovery.class).size()==amountOfDiscoveriesBeforeAdd);
+    }
+    @Test
+    public void shouldAddDiscoveryWhenIsNotNull(){
+        createDiscoveryServiceObjectWithInMemoryLayer();
+        long id=1234;
+        Discovery discovery=new Discovery();
+        discovery.setId(id);
+        int amountOfDiscoveryBeforeAdd=discRepo.getAll("",Discovery.class).size();
+
+        discoveryService.addDiscovery(discovery);
+        Discovery addedDiscovery=discRepo.get(Discovery.class,id);
+
+        assertTrue(discRepo.getAll("",Discovery.class).size()==amountOfDiscoveryBeforeAdd+1);
+        assertTrue(addedDiscovery!=null);
+        assertTrue(addedDiscovery==discovery);
+        assertTrue(addedDiscovery.getUpVote()==0);
+        assertTrue(addedDiscovery.getDownVote()==0);
+        assertTrue(addedDiscovery.getTimestamp()!=null);
+    }
     @Test
     public void shouldExecuteMethodAddOfDiscoveryRepository() throws Exception {
         Discovery discovery=mock(Discovery.class);
@@ -78,9 +115,19 @@ public class DiscoveryServiceTest {
     }
 
     @Test
+    public void shouldExecuteGetWithLimitOfDiscoveryRepository() throws Exception {
+        createDiscoveryServiceObjectWithInMemoryLayer();
+        List<Discovery> listOfAllDiscoveries=discRepo.getAll("",Discovery.class);
+        int begin=listOfAllDiscoveries.size()-8;
+        int quantity=5;
+
+        assertTrue(listOfAllDiscoveries.subList(begin,quantity).equals(discoveryService.getWithLimit(begin,quantity)));
+    }
+
+    @Test
     public void shouldExecuteGetMethodOfDiscoveryRepository() throws Exception {
-        discoveryService.getById(anyLong());
-        verify(discRepo).get(anyLong());
+        discoveryService.getById(1);
+        verify(discRepo,times(1)).get(eq(Discovery.class),anyLong());
     }
 
     public Object[] name(){
@@ -96,33 +143,44 @@ public class DiscoveryServiceTest {
     }
 
     @Test
-    public void shouldDoNothingAndReturnNullIfNameIsNull(){
+    public void shouldDoNotExecuteGetByNameIfNameIsNull(){
         discoveryService.getByName(null);
         verify(discRepo,never()).getByName(null);
         verify(discRepo,never()).getByName(anyString());
-        assertEquals(null,discoveryService.getByName(null));
     }
 
     @Test
-    public void shouldDoNothingAndReturnNullIfNameIsEmptyWord() throws Exception {
+    public void shouldDoNotExecuteGetByNameIfNameIsEmptyWord() throws Exception {
         String name="";
         discoveryService.getByName(name);
         verify(discRepo,never()).getByName(null);
         verify(discRepo,never()).getByName(name);
         verify(discRepo,never()).getByName(anyString());
-        assertEquals(null,discoveryService.getByName(name));
+    }
+
+    public Object[] discoveryName(){
+        return $("name","null","NAME","name name");
+    }
+    @Test
+    @Parameters(method = "discoveryName")
+    public void shouldReturnDiscoveryIfNameIsNotNullAndNotEmptyWord(String name){
+        createDiscoveryServiceObjectWithInMemoryLayer();
+        Discovery discovery=new Discovery();
+        discovery.setName(name);
+        discRepo.add(discovery);
+
+        assertEquals(discovery,discoveryService.getByName(name).get(0));
     }
 
     public Object[] discoveryId(){
         return $(1,1000,123456,987654321);
     }
-
     @Test
     @Parameters(method = "discoveryId")
     public void shouldUseTheSameLong(long id) throws Exception {
         discoveryService.getById(id);
-        verify(discRepo,times(1)).get(id);
-        verify(discRepo,never()).get(not(eq(id)));
+        verify(discRepo,times(1)).get(Discovery.class,id);
+        verify(discRepo,never()).get(eq(Discovery.class),not(eq(id)));
     }
 
     public Object[] validNamesOfComparators() {
@@ -139,40 +197,46 @@ public class DiscoveryServiceTest {
     public Object[] invalidNamesOfComparators() {
         return $( "date","comparator","Time","Popular","TIME","POPULAR",null);
     }
-
     @Test
     @Parameters(method ="invalidNamesOfComparators")
     public void ShouldExecuteMethodWithoutComparatorWhenNameOfComparatorIsInvalid(String names) throws Exception{
         discoveryService.getAll(names);
         verify(discRepo,never()).getAll(isA(Comparator.class));
-        verify(discRepo).getAll();
+        verify(discRepo).getAll(anyString(),eq(Discovery.class));
     }
 
     @Test
     public void shouldExecuteGetAllMethodWithTimeComparator() throws Exception{
-        discoveryService.getAll("time");
-        verify(discRepo).getAll(isA(DiscoveryService.TimeComparator.class));
+        createDiscoveryServiceObjectWithInMemoryLayer();
+
+        List<Discovery> listOfDiscoveries=discRepo.getAll("",Discovery.class);
+        listOfDiscoveries.sort(new DiscoveryService.TimeComparator());
+
+        assertEquals(listOfDiscoveries,discoveryService.getAll("time"));
     }
     @Test
     public void shouldExecuteGetAllMethodWithPopularComparator() throws Exception{
         discoveryService.getAll("popular");
-        verify(discRepo).getAll(isA(DiscoveryService.PopularComparator.class));
+        List<Discovery> listOfDiscoveries=discRepo.getAll("",Discovery.class);
+        listOfDiscoveries.sort(new DiscoveryService.PopularComparator());
+
+        assertEquals(listOfDiscoveries,discoveryService.getAll("popular"));
     }
 
     @Test
     public void shouldReturnListWithTheSameOrderAfterSortByTime() throws Exception{
         Discovery discovery1=new Discovery();
-        discovery1.setTimestamp(Timestamp.valueOf("2001-01-01 00:00:00"));
+        discovery1.setTimestamp(Timestamp.valueOf("2001-01-01 08:00:00"));
         Discovery discovery2=new Discovery();
-        discovery2.setTimestamp(Timestamp.valueOf("2002-01-01 00:00:00"));
+        discovery2.setTimestamp(Timestamp.valueOf("2001-01-01 09:00:00"));
         Discovery discovery3=new Discovery();
-        discovery3.setTimestamp(Timestamp.valueOf("2003-01-01 00:00:00"));
-        List<Discovery> listWithVelidOrder= Arrays.asList(discovery1,discovery2,discovery3);
+        discovery3.setTimestamp(Timestamp.valueOf("2001-01-01 10:00:00"));
+        List<Discovery> listWithValidOrder= Arrays.asList(discovery1,discovery2,discovery3);
         List<Discovery> listOrderdByTimeCompartor= Arrays.asList(discovery3,discovery1,discovery2);
 
-        assertTrue(!(listOrderdByTimeCompartor.equals(listWithVelidOrder)));
+        assertTrue(!(listOrderdByTimeCompartor.equals(listWithValidOrder)));
         listOrderdByTimeCompartor.sort(new DiscoveryService.TimeComparator());
-        assertTrue(listOrderdByTimeCompartor.equals(listWithVelidOrder));
+        assertTrue(listOrderdByTimeCompartor.equals(listWithValidOrder));
 
 
     }
@@ -198,14 +262,28 @@ public class DiscoveryServiceTest {
     }
 
     @Test
-    public void shouldExecuteMethodGetRemoveByDiscoveryIdAndRemove() throws Exception {
-        Discovery discovery=mock(Discovery.class);
+    public void shouldRemoveDiscoveryAndVote() throws Exception {
+        createDiscoveryServiceObjectWithInMemoryLayer();
 
-        when(discRepo.get(anyLong())).thenReturn(discovery);
-        discoveryService.removeDiscoveryById(anyLong());
+        Discovery discovery=new Discovery();
+        discovery.setId(1234);
+        discRepo.add(discovery);
+        int amountOfdiscoveriesBeforeRemove=discRepo.getAll("",Discovery.class).size();
+        Vote vote= new Vote();
+        vote.setId(4321);
+        vote.setDiscovery(discovery);
+        voteRepo.add(vote);
+        int amountOfVotesBeforeRemove=voteRepo.getAll("",Vote.class).size();
 
-        verify(voteRepo).removeByDiscoveryId(anyLong());
-        verify(discRepo).remove(discovery);
+        assertEquals(discovery,discRepo.get(Discovery.class,1234));
+        assertEquals(vote,voteRepo.get(Vote.class,4321));
+
+        discoveryService.removeDiscoveryById(1234);
+
+        assertEquals(amountOfdiscoveriesBeforeRemove-1,discRepo.getAll("",Discovery.class).size());
+        assertEquals(null,discRepo.get(Discovery.class,1234));
+        assertEquals(amountOfVotesBeforeRemove-1,voteRepo.getAll("",Vote.class).size());
+        assertEquals(null,voteRepo.get(Vote.class,4321));
 
     }
 
@@ -217,17 +295,9 @@ public class DiscoveryServiceTest {
     @Parameters(method = "discoveryIdRemove")
     public void methodsShouldUseTheSameValueOfId(long id) throws Exception {
         discoveryService.removeDiscoveryById(id);
-        verify(discRepo,never()).remove(isA(Discovery.class));
-        verify(discRepo,never()).get(not(eq(id)));
-        verify(voteRepo,never()).removeByDiscoveryId(not(eq(id)));
-        verify(discRepo,times(1)).get(eq(id));
-        verify(voteRepo,times(1)).removeByDiscoveryId(eq(id));
-    }
-    @Test
-    public void shouldExecuteMethodUpdate() throws Exception {
-        Discovery discovery=mock(Discovery.class);
-        discoveryService.updateDiscovery(discovery);
-        verify(discRepo).update(discovery);
 
+        verify(discRepo,times(1)).get(Discovery.class,id);
+        verify(voteRepo,times(1)).removeByDiscoveryId(id);
     }
+
 }
