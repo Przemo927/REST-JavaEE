7d1eddca0e828e79a7bb69561300a02d074eb04e
Przemek <przemo927@wp.pl>
2018-08-12 16:47:05 +0200

    Adapt UserServiceTest.

--- projekt/src/main/test/pl/przemek/service/UserServiceTest.java
+++ projekt/src/main/test/pl/przemek/service/UserServiceTest.java
@@ -11,12 +11,14 @@ import pl.przemek.model.Role;
 import pl.przemek.model.User;
 import pl.przemek.repository.JpaRoleRepository;
 import pl.przemek.repository.JpaUserRepository;
+import pl.przemek.repository.inMemoryRepository.JpaRoleRepositoryInMemoryImpl;
+import pl.przemek.repository.inMemoryRepository.JpaUserRepositoryInMemoryImpl;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static junitparams.JUnitParamsRunner.$;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.AdditionalMatchers.not;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -38,28 +40,108 @@ public class UserServiceTest {
 
     }
     @Test
-    public void shouldExecuteMethodAddOfUserRepository() throws Exception {
-        User user=mock(User.class);
-        when(user.getPassword()).thenReturn(anyString());
+    public void shouldAddUser() throws Exception {
+        createUserServiceObjectWithInMemoryLayer();
+        String password="password";
+        long id=1234;
+        int amountOfusers=userRepo.getAll("",User.class).size();
+        User user=new User();
+        user.setPassword(password);
+        user.setId(id);
+
+        assertTrue(userRepo.get(User.class,id)==null);
+
         userService.addUser(user);
-        verify(userRepo).add(user);
+
+        assertEquals(userRepo.getAll("",User.class).size(),amountOfusers+1);
+        assertTrue(userRepo.get(User.class,id)!=null);
+        assertEquals(userRepo.get(User.class,id).getId(),id);
+    }
+    @Test
+    public void shouldAddUserToRole() throws Exception {
+        createUserServiceObjectWithInMemoryLayer();
+        User user=new User();
+        String name="Name";
+        user.setUsername(name);
+        assertEquals(0,rolRepo.getRoles("user").get(0).getUsers().size());
+
+        userService.addUser(user);
+
+        Role userRole=rolRepo.getRoles("user").get(0);
+        assertEquals(1,userRole.getUsers().size());
+        assertTrue(userRole.getUsers().contains(user));
+
+    }
+    @Test
+    public void shouldDoNotAddUserAndRoleWhenUserIsNull() throws Exception {
+        createUserServiceObjectWithInMemoryLayer();
+        int amountOfusers=userRepo.getAll("",User.class).size();
+        List<User> listOfUsers=userRepo.getAll("",User.class);
+
+        userService.addUser(null);
+
+        assertEquals(userRepo.getAll("",User.class).size(),amountOfusers);
+        assertEquals(listOfUsers,userRepo.getAll("",User.class));
+        assertEquals(0,rolRepo.getRoles("user").get(0).getUsers().size());
+    }
+    @Test
+    public void shouldNotAddUserToRoleWhenRoleDidNotFind() throws Exception {
+        createUserServiceObjectWithInMemoryLayer();
+        ((JpaRoleRepositoryInMemoryImpl)rolRepo).deleteRoleByRoleName("user");
+        User user=new User();
+        user.setId(1234);
+
+        userService.addRole(user);
+
+        assertTrue(rolRepo.getRoles("user").isEmpty());
+
     }
 
     @Test
-    public void shouldReturnNullWhenUserIsNull() throws Exception {
-        assertEquals(null,userService.addUser(null));
+    public void shouldExecuteGetMethodOfUserRepository(){
+        createUserServiceObjectWithInMemoryLayer();
+        long id=1234;
+        User user=new User();
+        user.setId(id);
+
+        assertEquals(null,userService.getUserById(id));
+        userRepo.add(user);
+
+        assertEquals(user,userService.getUserById(id));
+    }
+    @Test
+    public void shouldNotRemoveUserIfDidNotSave(){
+        createUserServiceObjectWithInMemoryLayer();
+        User user=new User();
+        user.setId(1234);
+        int amountOfUsers=userRepo.getAll("",User.class).size();
+        assertEquals(null,userService.getUserById(1234));
+
+        userService.removeByUserId(1234);
+
+        assertEquals(amountOfUsers,userRepo.getAll("",User.class).size());
     }
 
     @Test
-    public void ShouldUseTheSameObjectOfUser() throws Exception {
-        User user=mock(User.class);
+    public void shouldRemoveUserIfSaved(){
+        createUserServiceObjectWithInMemoryLayer();
+        long id=1234;
+        User user=new User();
+        user.setId(id);
+        userRepo.add(user);
+        int amountOfUsers=userRepo.getAll("",User.class).size();
 
-        when(user.getPassword()).thenReturn(anyString());
-        userService.addUser(user);
+        userService.removeByUserId(id);
 
-        verify(userRepo).add(user);
-        verify(userRepo,never()).add(not(eq(user)));
+        assertEquals(amountOfUsers-1,userRepo.getAll("",User.class).size());
+        assertEquals(null,userRepo.get(User.class,id));
     }
+    private void createUserServiceObjectWithInMemoryLayer(){
+        userRepo=new JpaUserRepositoryInMemoryImpl();
+        rolRepo=new JpaRoleRepositoryInMemoryImpl();
+        userService=new UserService(userRepo,rolRepo);
+    }
+
 
     @Test
     public void shouldNotExecuteMethodUpdateOfRoleRepositoryWhenListOfRolesIsEmpty() throws Exception {
@@ -87,8 +169,8 @@ public class UserServiceTest {
 
     @Test
     public void shouldExecuteMethodGetOfUserRepository() throws Exception{
-            userService.getUserById(anyLong());
-            verify(userRepo).get(anyLong());
+            userService.getUserById(1);
+            verify(userRepo).get(eq(User.class),anyLong());
     }
 
     public Object[] userId(){
@@ -98,15 +180,15 @@ public class UserServiceTest {
     @Parameters(method = "userId")
     public void shoudlUseTheSameValueOfId(long id) throws Exception{
         userService.getUserById(id);
-        verify(userRepo,times(1)).get(id);
-        verify(userRepo,never()).get(not(eq(id)));
+        verify(userRepo,times(1)).get(User.class,id);
+        verify(userRepo,never()).get(eq(User.class),not(eq(id)));
     }
 
     @Test
     public void shouldExecuteMethodRemoveOfUserRepository() throws Exception{
         User user=mock(User.class);
-        when(userRepo.get(anyLong())).thenReturn(user);
-        userService.removeByUserId(anyLong());
+        when(userRepo.get(eq(User.class),anyLong())).thenReturn(user);
+        userService.removeByUserId(1);
         verify(userRepo).remove(user);
     }
 
@@ -114,14 +196,14 @@ public class UserServiceTest {
     @Parameters(method = "userId")
     public void shouldUseTheSameValueOfId(long id) throws Exception{
         userService.removeByUserId(id);
-        verify(userRepo).get(id);
-        verify(userRepo,never()).get(not(eq(id)));
+        verify(userRepo).get(User.class,id);
+        verify(userRepo,never()).get(eq(User.class),not(eq(id)));
     }
 
     @Test
     public void shouldDoNotExecuteRemoveMethodWhenUserWasNotFound() throws Exception {
-        when(userRepo.get(anyLong())).thenReturn(null);
-        userService.removeByUserId(anyLong());
+        when(userRepo.get(eq(User.class),anyLong())).thenReturn(null);
+        userService.removeByUserId(1);
         verify(userRepo,never()).remove(isA(User.class));
     }
 
@@ -135,10 +217,10 @@ public class UserServiceTest {
     @Test
     public void shouldExecuteMethodGetAllOfUserRepository(){
         userService.getAllUsers();
-        verify(userRepo).getAll();
+        verify(userRepo).getAll(anyString(),eq(User.class));
     }
 
-    public Object[] stringsBeforeAndAfterMd5Hashing(){
+    /*public Object[] stringsBeforeAndAfterMd5Hashing(){
         return $(new String[] {"","d41d8cd98f00b204e9800998ecf8427e"},
                 new String[] {"a","cc175b9c0f1b6a831c399e269772661"},
                 new String[] {"abc","900150983cd24fb0d6963f7d28e17f72"},
@@ -151,7 +233,7 @@ public class UserServiceTest {
     @Parameters(method = "stringsBeforeAndAfterMd5Hashing")
     public void shouldEqualsStringAfterHashingAndResultOfEncryptPasswordMethodWithStringBeforeHashing(String beforeHashing,String afterHashing){
         assertEquals(afterHashing,userService.encryptPassword(beforeHashing));
-    }
+    }*/
 
 
 }
