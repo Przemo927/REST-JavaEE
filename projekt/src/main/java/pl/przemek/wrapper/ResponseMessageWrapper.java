c06c852bae9e5de25f1558d04efd4de00fd2ce16
Przemek <przemo927@wp.pl>
2018-08-26 14:45:05 +0200

    Change wrapMessage method as static.

--- projekt/src/main/java/pl/przemek/wrapper/ResponseMessageWrapper.java
+++ projekt/src/main/java/pl/przemek/wrapper/ResponseMessageWrapper.java
@@ -3,7 +3,7 @@ package pl.przemek.wrapper;
 import org.json.simple.JSONObject;
 
 public class ResponseMessageWrapper {
-    public JSONObject wrappMessage(String message) {
+    public static JSONObject wrappMessage(String message) {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("Message", message);
         return jsonObject;
