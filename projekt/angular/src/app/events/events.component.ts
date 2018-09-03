95659642f9a941302ba1613b7e64b2a61d83fe05
Przemek <przemo927@wp.pl>
2018-09-03 21:12:53 +0200

    Use app-header in events component.

--- projekt/angular/src/app/events/events.component.ts
+++ projekt/angular/src/app/events/events.component.ts
@@ -15,27 +15,32 @@ export class EventsComponent implements OnInit {
   private cityStyle;
   private positionStyle;
   private allStyle;
+  private title:string;
 
 
 ngOnInit() {
-  this.positionStyle={'opacity':'0.5'};
+  this.positionStyle={'opacity':'1'};
   this.cityStyle={'opacity':'0.5'};
-  this.allStyle={'opacity':'1'};
+  this.allStyle={'opacity':'0.5'};
+  this.title='Search events By Positon';
 }
   searchByPosition():void {
     this.positionStyle={'opacity':'1'};
     this.cityStyle={'opacity':'0.5'};
     this.allStyle={'opacity':'0.5'};
+    this.title='Search events By Positon';
   }
   searchByCity():void {
     this.positionStyle={'opacity':'0.5'};
     this.cityStyle={'opacity':'1'};
     this.allStyle={'opacity':'0.5'};
+    this.title='Search events by city';
   }
   showAllEvents():void {
     this.positionStyle={'opacity':'0.5'};
     this.cityStyle={'opacity':'0.5'};
     this.allStyle={'opacity':'1'};
+    this.title='All events';
   }
 
 getEventsByNameOfCity(nameOfCity:string){
