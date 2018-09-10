d3827ed4f15b7385da1eb1cfb69663f7dfd6f928
Przemek <przemo927@wp.pl>
2018-09-10 18:40:34 +0200

    Save logger to file.

--- projekt/src/main/java/pl/przemek/config/CDIconfig.java
+++ projekt/src/main/java/pl/przemek/config/CDIconfig.java
@@ -2,12 +2,33 @@ package pl.przemek.config;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class CDIconfig {
+    private static FileHandler fh;
+    private static Logger logger;
+    private final static String LOG_PATH=System.getProperty("user.dir")+"\\log\\ProjektLog.log";
 
     @Produces
     public Logger produceLogger(InjectionPoint injectionPoint) {
-        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+        try {
+            if(logger==null){
+                logger = Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+            }
+            if(fh==null){
+                fh = new FileHandler(LOG_PATH);
+                fh.setLevel(Level.ALL);
+                logger.addHandler(fh);
+                SimpleFormatter formatter = new SimpleFormatter();
+                fh.setFormatter(formatter);
+            }
+        } catch (SecurityException | IOException e) {
+            logger.log(Level.SEVERE,"Logger exception",e);
+        }
+        return logger;
     }
 }
