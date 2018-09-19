d998f75d4c623aa27414e2e2cd91faeeac4e85f0
Przemek <przemo927@wp.pl>
2018-09-19 18:30:17 +0200

    Add functionality which allow refresh page when route was changed.

--- projekt/angular/src/app/discovery/discovery.component.ts
+++ projekt/angular/src/app/discovery/discovery.component.ts
@@ -2,7 +2,7 @@ import {Component, ContentChild, ElementRef, OnInit, Output, ViewChild} from "@a
 import {DiscoveryService} from "../discovery.service";
 import {CommentService} from "../comment.service";
 import {Discovery} from "../discovery";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
 import {Comment} from "../comment";
 import {ValidatabletextareaComponent} from "../validatabletextarea/validatabletextarea.component";
 import {UrlUtils} from "../UrlUtils";
@@ -21,33 +21,43 @@ export class DiscoveryComponent implements OnInit {
   comment: Comment=new Comment();
   VOTE_UP='VOTE_UP';
   VOTE_DOWN='VOTE_DOWN';
-  message:string;
+  message:string='';
+  id:number;
 
   constructor(private discoveryService: DiscoveryService,private route: ActivatedRoute,
-              private commentService: CommentService) {}
+              private commentService: CommentService, private router:Router) {
+    router.events.forEach((e) => {
+      if(e instanceof NavigationEnd){
+        this.id = +this.route.snapshot.paramMap.get('id');
+        this.getDiscovery();
+        this.getCommentsById();
+      }
+    });
+  }
 
+  ngOnInit() {
+    this.id = +this.route.snapshot.paramMap.get('id');
+    this.getDiscovery();
+    this.getCommentsById();
+  }
 
   getDiscovery(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.discoveryService.getDiscovery(id).subscribe(discovery=>this.discovery=discovery);
+    this.discoveryService.getDiscovery(this.id).subscribe(discovery=>this.discovery=discovery);
   }
 
   getCommentsById(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.commentService.getCommentsById(id).subscribe(comments=> this.comments=comments);
+    this.commentService.getCommentsById(this.id).subscribe(comments=> this.comments=comments);
   }
 
   addComment():void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.commentService.addComment(this.comment,id).subscribe(response=>{
-      this.message='';
+    this.commentService.addComment(this.comment,this.id).subscribe(response=>{
+      if(response!==null && response.InvalidFieldList!==undefined){
+        this.message=response.InvalidFieldList.comment;
+        this.messageRef.setWrong();
+        this.messageRef.setValidationMessage(this.message);
+      }else {
         this.getCommentsById();
-        let textArea=document.getElementsByName("comment")[0];
-        if(response!==null && response.InvalidFieldList!==undefined){
-          this.message=response.InvalidFieldList.comment;
-          this.messageRef.setWrong();
-          this.messageRef.setValidationMessage(this.message);
-        }
+      }
     });
   }
 
@@ -59,15 +69,6 @@ export class DiscoveryComponent implements OnInit {
     window.location.href='http://localhost:8080/projekt/api/votecom?vote='+voteType+'&commentId='+id;
     setTimeout(()=>
       this.getCommentsById(),500);
-  }
-
-  ngOnInit() {
-    let urlAddVote=UrlUtils.generateBasicUrl(window.location.protocol);
-    urlAddVote=UrlUtils.addParameterToUrl(urlAddVote,'vote','VOTE_UP');
-    urlAddVote=UrlUtils.addParameterToUrl(urlAddVote,'commentId',1);
-    console.log(urlAddVote);
-    this.getDiscovery();
-    this.getCommentsById();
   }
 
   assignComment(comment:string){
