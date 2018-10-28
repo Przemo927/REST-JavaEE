d0dc76e89bf5b92c53371075aff5f5a084303dd5
Przemek <przemo927@wp.pl>
2018-10-28 22:06:27 +0100

    Make variable final.

--- projekt/src/main/java/pl/przemek/Message/EmailMessageTemplate.java
+++ projekt/src/main/java/pl/przemek/Message/EmailMessageTemplate.java
@@ -1,20 +1,16 @@
 package pl.przemek.Message;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.security.CodeSource;
-
 public class EmailMessageTemplate {
-    private static String message="Click link below to continue registration"+
+    private final static String MESSAGE_TEMPLATE="Click link below to continue registration"+
             "<br><br><a href=[baseUrl]register/[user]>Continue</a>";
     private final static String  URL_SEPARATOR="/";
 
     public static String getPreparedMessage(String baseUrl,String username){
         String temporaryMessage;
         if(baseUrl.endsWith(URL_SEPARATOR)) {
-            temporaryMessage=message.replace("[baseUrl]",baseUrl);
+            temporaryMessage=MESSAGE_TEMPLATE.replace("[baseUrl]",baseUrl);
         }else {
-            temporaryMessage=message.replace("[baseUrl]",baseUrl+URL_SEPARATOR);
+            temporaryMessage=MESSAGE_TEMPLATE.replace("[baseUrl]",baseUrl+URL_SEPARATOR);
         }
 
         return temporaryMessage.replace("[user]",username);
