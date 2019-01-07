f771c32346919676015a8ffd81e5684222f26c59
Przemek <przemo927@wp.pl>
2019-01-07 21:28:17 +0100

    Use File.separator instead string literal.

--- projekt/src/main/java/pl/przemek/config/CDIconfig.java
+++ projekt/src/main/java/pl/przemek/config/CDIconfig.java
@@ -2,6 +2,7 @@ package pl.przemek.config;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
@@ -9,9 +10,11 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class CDIconfig {
+
+    private static final String NAME_LOG_FILE="ProjektLog.log";
     private static FileHandler fh;
     private static Logger logger;
-    private final static String LOG_PATH=System.getProperty("user.dir")+"\\log\\ProjektLog.log";
+    private final static String LOG_PATH=System.getProperty("user.dir")+ File.separator+"log"+File.separator+NAME_LOG_FILE;
 
     @Produces
     public Logger produceLogger(InjectionPoint injectionPoint) {
