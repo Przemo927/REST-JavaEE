7a18b04afaf16ff76e1051d707b7cd982e88cd04
Przemek <przemo927@wp.pl>
2019-01-10 16:52:50 +0100

    Refactor code of ValidationMapper

--- projekt/src/main/java/pl/przemek/mapper/ValidatorMapper.java
+++ projekt/src/main/java/pl/przemek/mapper/ValidatorMapper.java
@@ -1,33 +1,35 @@
 package pl.przemek.mapper;
 
-import java.util.*;
+import org.jboss.resteasy.api.validation.ResteasyViolationException;
+import org.json.simple.JSONObject;
+import pl.przemek.Utils.StringUtils;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ValidationException;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-
-import org.jboss.resteasy.api.validation.ResteasyViolationException;
-import org.json.simple.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 @Provider
+@SuppressWarnings("unchecked")
 public class ValidatorMapper implements ExceptionMapper<ValidationException> {
 
     @Override
     public Response toResponse(ValidationException e) {
-        JSONObject json = new JSONObject();
-        Map<String, String> map = new HashMap<>();
     	if(e instanceof ResteasyViolationException) {
             ResteasyViolationException re = (ResteasyViolationException) e;
             Set<ConstraintViolation<?>> violations = re.getConstraintViolations();
+            Map<String, String> map = new HashMap<>();
+            JSONObject json = new JSONObject();
             for (ConstraintViolation<?> v : violations) {
-                String[] strings = (v.getPropertyPath() + "").split("\\.");
+                String[] strings = (v.getPropertyPath().toString()).split("\\"+StringUtils.DOT);
                 map.put(strings[2],v.getMessage());
-        json.put("invalidFieldList", map);
-        }
+                json.put("invalidFieldList", map);
+            }
             return Response.status(Status.BAD_REQUEST).entity(json).build();
     	}
 		return Response.status(Status.BAD_REQUEST).build();
