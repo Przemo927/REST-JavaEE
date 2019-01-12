e9fb2fb97fb832737a432788b6ec01f3241f9d0a
Przemek <przemo927@wp.pl>
2019-01-12 11:19:42 +0100

    Add StringUtils class.

---
+++ projekt/src/main/java/pl/przemek/Utils/StringUtils.java
@@ -0,0 +1,8 @@
+package pl.przemek.Utils;
+
+public class StringUtils {
+    public static final String LEFT_SQUARE_BRACKET="[";
+    public static final String RIGHT_SQUARE_BRACKET="]";
+    public static final String ROUND_BRACKETS="()";
+    public static final String DOT=".";
+}
