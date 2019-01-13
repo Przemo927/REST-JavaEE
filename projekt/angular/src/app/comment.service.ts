7a6af90143ad44478a2a5d86ccec63fea165adbd
Przemek <przemo927@wp.pl>
2019-01-13 21:22:08 +0100

    Use ErrorHandler instead own method to handle errors.

--- projekt/angular/src/app/comment.service.ts
+++ projekt/angular/src/app/comment.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { catchError, tap } from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
 import { Comment } from './comment';
+import {ErrorHandler} from "./errorhandler";
 
 
 @Injectable()
@@ -13,34 +14,17 @@ export class CommentService {
 
   private commentUrl='http://localhost:8080/projekt/api/comment';
 
-
-  private handleError<T> (operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
-
-    // TODO: better job of transforming error for user consumption
-    console.log(`${operation} failed: ${error.message}`);
-
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
-
-
-
   getComments(): Observable<Comment[]> {
   return this.http.get<Comment[]>(this.commentUrl)
   .pipe(tap(comment => console.log(`fetched comments`)),
-      catchError(this.handleError<Comment[]>('getComments',[]))
+      catchError(ErrorHandler.handleError<Comment[]>('getComments',[]))
     );
   }
 
   getCommentsById(id:number):Observable<Comment[]>{
     return this.http.get<Comment[]>(this.commentUrl+'/'+id)
     .pipe(tap(comment=>console.log('fetched comments for discovery id='+id)),
-    catchError(this.handleError<Comment[]>('getCommentById',[]))
+    catchError(ErrorHandler.handleError<Comment[]>('getCommentById',[]))
     );
   }
 
@@ -48,7 +32,7 @@ export class CommentService {
    addComment (comment: Comment,id:number): Observable<any> {
   return this.http.post<any>(this.commentUrl+'/'+id,comment).pipe(
     tap(comment => console.log('Comment added')),
-    catchError(this.handleError<any>('addComment'))
+    catchError(ErrorHandler.handleError<any>('addComment'))
   );
 }
 }
