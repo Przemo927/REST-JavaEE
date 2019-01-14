527c0c81f93e2d91cb6901367a323725eb7700d5
Przemek <przemo927@wp.pl>
2019-01-14 18:32:31 +0100

    Add new endpoint page.

--- projekt/angular/src/app/endpoint.enum.ts
+++ projekt/angular/src/app/endpoint.enum.ts
@@ -2,5 +2,6 @@ export enum EndPoint {
   discoveries = "api/discovery",
   encryptDiscoveries = "api/discovery?encrypt=yes",
   voteDiscovery = "api/vote",
+  voteComment = "api/votecom",
   logout = "api/logout",
 }
