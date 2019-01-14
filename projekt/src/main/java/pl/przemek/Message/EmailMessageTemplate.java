da6cb3598195c72781cef698d66191746e0d5f9f
Przemek <przemo927@wp.pl>
2019-01-14 21:17:52 +0100

    use static variable form ResponseUtils instead literal

--- projekt/src/main/java/pl/przemek/Message/EmailMessageTemplate.java
+++ projekt/src/main/java/pl/przemek/Message/EmailMessageTemplate.java
@@ -1,16 +1,17 @@
 package pl.przemek.Message;
 
+import pl.przemek.rest.utils.ResponseUtils;
+
 public class EmailMessageTemplate {
     private final static String MESSAGE_TEMPLATE="Click link below to continue registration"+
             "<br><br><a href=[baseUrl]register/[user]>Continue</a>";
-    private final static String  URL_SEPARATOR="/";
 
     public static String getPreparedMessage(String baseUrl,String username){
         String temporaryMessage;
-        if(baseUrl.endsWith(URL_SEPARATOR)) {
+        if(baseUrl.endsWith(ResponseUtils.URL_SEPARATOR)) {
             temporaryMessage=MESSAGE_TEMPLATE.replace("[baseUrl]",baseUrl);
         }else {
-            temporaryMessage=MESSAGE_TEMPLATE.replace("[baseUrl]",baseUrl+URL_SEPARATOR);
+            temporaryMessage=MESSAGE_TEMPLATE.replace("[baseUrl]",baseUrl+ ResponseUtils.URL_SEPARATOR);
         }
 
         return temporaryMessage.replace("[user]",username);
